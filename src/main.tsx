a534f0a7c3bf2a3455fdd3c5ab5ce27566186176
yunkbaza <allanbamirati@live.com>
2024-09-12 14:35:14 -0300

    Instalei o router-dom e mexi na main.tsx para encaixar os paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./components/routes/Error.tsx"
+import AreaCliente from './components/routes/AreaCliente.tsx';
 import App from './App.tsx'
+import CriarConta from './components/routes/CriarConta.tsx';
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "areacliente",
+    element: <AreaCliente />,
+  },
+  {
+    path: "criarconta",
+    element: <CriarConta />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
